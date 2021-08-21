4d9cd9a9c690660c37ad45e8bf9f025147b1f6ea
kulendu <imkulendu@gmail.com>
2021-08-22 01:59:55 +0530

    Fixing the code structure.

--- src/com/kulendu/hello_world/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package com.kulendu.hello_world;
-
-public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("First program for my Java Lab class");
-    }
-}
